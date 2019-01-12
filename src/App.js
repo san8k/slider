77e9be929c5e6e4f389f98f66709337481343521
Alexander <alrttv@gmail.com>
2019-01-12 16:05:29 +0300

    Page components added

--- src/App.js
+++ src/App.js
@@ -1,30 +1,53 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import Grid from 'react-bootstrap/lib/Grid';
-import Row from 'react-bootstrap/lib/Row';
-import Col from 'react-bootstrap/lib/Col';
 import styled from 'styled-components'
+import Header from './components/Header';
+import Main from './components/Main';
+import Menu from './components/Menu';
+
+const url = process.env.PUBLIC_URL + '/img/bg.jpg';
+
+const HeaderWrapper = styled.header`
+    width: 100%;
+    height: 49px;
+    background-color: #242424;
+    opacity: 0.8;
+`
+const MenuWrapper = styled.div`
+    height: 89px;
+    padding: 21px;
+`
+
+const MainWrapper = styled.main`
+    height: 600px;
+    padding-top: 130px;
+    background: url(${url}) no-repeat;
+    background-size: cover;
+`
 
 class App extends Component {
   render() {
     return (
-      <div className="wrapper">
-        <SayFullName name="Lisff" surname="Urr" link="#" />
-        <SayFullName name="Kor" surname="Pdd" link="#" />
-        <SayFullName name="Abc" surname="Acc" link="#" />
-      </div>
-      
+      <div className="App">
+        <HeaderWrapper>
+            <Grid>
+                <Header />
+            </Grid>
+        </HeaderWrapper>
+        <MenuWrapper>
+            <Grid>
+                <Menu />
+            </Grid>
+        </MenuWrapper>
+        <MainWrapper>
+            <Grid>
+                <Main />
+            </Grid>
+        </MainWrapper>
+    </div>      
     );
   }
-}
-
-function SayFullName(props) {
-  return (<div>
-      <h2>Мое имя: {props.name}</h2>
-      <h2>Моя фамилия: {props.surname}</h2>
-      <a href={props.link}>Мой профиль</a>
-  </div>)
 }
 
 export default App;
