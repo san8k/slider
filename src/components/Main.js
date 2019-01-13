2d6d99408616ab680659c634a9dbc8038eec6544
Alexander <alrttv@gmail.com>
2019-01-13 16:20:47 +0300

    Slider component added

--- src/components/Main.js
+++ src/components/Main.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import Row from 'react-bootstrap/lib/Row';
 import Col from 'react-bootstrap/lib/Col';
 import styled from 'styled-components'
+import Slider from './Slider';
 
 const Repair = styled.div`
     color: #fff;
@@ -54,7 +55,7 @@ class Main extends Component {
                     </CallButton>
                 </Col>
                 <Col lg={6} lgOffset={1}>
-
+                    <Slider />
                 </Col>
             </Row>
         );
